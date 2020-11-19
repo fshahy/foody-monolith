a89891a41ade9de00abd44e8edf92e32180fc0ed
Farid Shahy <fshahy@gmail.com>
2020-11-19 15:49:13 +0330

    init maven project.

---
+++ src/main/java/ir/fshahy/foody/FoodyApplication.java
@@ -0,0 +1,13 @@
+package ir.fshahy.foody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ir/fshahy/foody/FoodyApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.fshahy.foody;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
